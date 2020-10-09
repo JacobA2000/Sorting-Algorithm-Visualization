d553759b6710e3a761e999fe19a408705d1be88e
JacobA2000 <jacoballen785@gmail.com>
2020-10-10 00:43:46 +0100

    Added colours for insertion sort and added function header comments

--- sorting.js
+++ sorting.js
@@ -3,6 +3,13 @@ let w = 10;
 let states = [];
 let playbackSpeed = 100
 
+/*
+FUNCTION:       SETUP
+PARAMETERS:     NONE
+
+DESCRIPTION:    SETS UP THE CANVAS, CREATES AN ARRAY FULL OF RANDOM NUMBERS WITH IT'S LENGTH
+                BASED ON SCREEN WIDTH, AND SETS THE STATE OF EACH ITEM IN THE ARRAY.          
+*/
 function setup(){
     createCanvas(windowWidth -200, windowHeight -200);
     arrayToSort = new Array(floor(width/w));
@@ -12,6 +19,13 @@ function setup(){
     }
 }
 
+/*
+FUNCTION:       RUN
+PARAMETERS:     NONE
+
+DESCRIPTION:    GETS THE SELECTED ALGORITHM AND SPEED, AND RUNS 
+                THE SELECTED ALGORITHM AT THAT SPEED.       
+*/
 function run(){
     var algorithmSelectBox = document.getElementById("algorithm-selection");
     var selectedAlgorithm = parseInt(algorithmSelectBox.options[algorithmSelectBox.selectedIndex].value);
@@ -34,6 +48,12 @@ function run(){
     }
 }
 
+
+/*
+FUNCTION:       DRAW
+PARAMETERS:     NONE
+DESCRIPTION:    DRAWS THE GRAPH       
+*/
 function draw(){
     background(0, 0, 0);
 
@@ -51,6 +71,14 @@ function draw(){
     }
 }
 
+/*
+FUNCTION:       SWAP
+PARAMETERS:     arr: The array to swap, a: The first value for the swap, 
+                b: The second value for the swap.
+
+DESCRIPTION:    SETS UP THE CANVAS, CREATES AN ARRAY FULL OF RANDOM NUMBERS WITH IT'S LENGTH
+                BASED ON SCREEN WIDTH, AND SETS THE STATE OF EACH ITEM IN THE ARRAY.          
+*/
 async function swap(arr, a, b){
     await sleep(playbackSpeed)
 
@@ -59,19 +87,35 @@ async function swap(arr, a, b){
     arr[b] = temp;
 }
 
+/*
+FUNCTION:       BUBBLESORT
+PARAMETERS:     arrayToSort: The array we wish to sort.
+
+DESCRIPTION:    PERFORMS THE BUBBLESORT ALGORITHM TO SORT THE ARRAY IN ASCENDING ORDER.        
+*/
 async function bubbleSort(arrayToSort){ 
     for (let i = 0; i < (arrayToSort.length - 1); i++){
         for(let j=0; j < (arrayToSort.length - i - 1); j++){
             states[j] = 0;
+            states[j+1] = 1
             if(arrayToSort[j] > arrayToSort[j+1]){
                 await swap(arrayToSort, j, j+1);
             }
             states[j] = -1;
+            states[j+1] = -1 
         }
     }   
     console.log(arrayToSort)
 }
 
+/*
+FUNCTION:       QUICKSORT
+PARAMETERS:     arr: The array to swap, start: The start of the partition, 
+                end: The end of the partition
+
+DESCRIPTION:    PERFORMS THE QUICKSORT ALGORITHM TO SORT IN ASCENDING 
+                ORDER USING LOMUTO PARTITIONING 
+*/
 async function quickSort(arr, start, end){
     if(start >= end){
         return;
@@ -83,7 +127,13 @@ async function quickSort(arr, start, end){
     await Promise.all([quickSort(arr, start, index-1), quickSort(arr, index+1, end)]);
 }
 
-//Lomuto Partitioning
+/*
+FUNCTION:       PARTITION
+PARAMETERS:     arr: The array to swap, start: The start of the partition, 
+                end: The end of the partition
+
+DESCRIPTION:    PARTITION FUNCTION USING LOMUTO PARTIONING TO SORT EACH PARTITION OF THE ARRAY
+*/
 async function partition(arr, start, end){
     for(let i = start; i < end; i++){
         states[i] = 1;
@@ -111,20 +161,36 @@ async function partition(arr, start, end){
     return pivotIndex;
 }
 
+/*
+FUNCTION:       INSERTIONSORT
+PARAMETERS:     arr: The array to sort. 
+
+DESCRIPTION:    PERFORMS THE INSERTION SORT ALGORITHM TO SORT THE ARRAY IN ASCENDING ORDER
+*/
 async function insertionSort(arr){
     for(let i = 1; i < arr.length; i++){
         let current = arr[i];
         let j = i-1;
 
         while((j > -1) && (current < arr[j])){
+            states[j] = 0
+            states[j-1] = 1
             await sleep(playbackSpeed);
             arr[j+1] = arr[j];
+            states[j] = -1
+            states[j-1] = -1
             j--;
         }
         arr[j+1] = current
     }
 }
 
+/*
+FUNCTION:       SLEEP
+PARAMETERS:     ms: The time to sleep in milliseconds
+
+DESCRIPTION:    ASYNCHRONOUSLY SLEEPS EXECUTION WHEN CALLED
+*/
 function sleep(ms){
     return new Promise(resolve => setTimeout(resolve, ms));
 }
